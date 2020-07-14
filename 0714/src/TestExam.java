ba4b6426dff99117dee6d356694e332993f97d57
lju <jungun1115@gmail.com>
2020-07-14 09:25:53 +0900

    hello

---
+++ 0714/src/TestExam.java
@@ -0,0 +1,20 @@
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1; i<=5; i++) {
+			System.out.print(i + "	");
+		};
+		
+		System.out.println();
+		
+		int l;
+		for(l=10; l>=6; l--) {
+			System.out.print(l + "	");
+		};
+
+	}
+
+}
